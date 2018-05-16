c45ea34e60c24388e4693beeea7e352cb9030c14
George <drg159@hotmail.com>
2018-05-16 22:36:17 +0300

    reactive form addition

--- src/app/modules/bugs/bug/bug.component.ts
+++ src/app/modules/bugs/bug/bug.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Bug } from "../models/bugs.model";
 import { BugsService } from "../bugs.service";
 import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-bug",
@@ -10,25 +11,34 @@ import { Router } from "@angular/router";
 })
 export class BugComponent implements OnInit {
 
-  bug: Bug = {
-    id: null,
-    title: "",
-    description: "",
-    priority: 0
-  };
+  bugFormGroup: FormGroup;
 
   constructor(private service: BugsService, private router: Router) { }
 
   ngOnInit() {
+    this.bugFormGroup = new FormGroup({
+      title: new FormControl("", Validators.required),
+      description: new FormControl("", Validators.required),
+      priority: new FormControl(null),
+      reporter: new FormControl(null),
+      status: new FormControl(null)
+    });
+
+    const statusControl = this.bugFormGroup.get("status");
+
+    this.bugFormGroup.get("reporter").valueChanges.subscribe((rep: string) => {
+      if (rep === "QA" && statusControl.value === null) {
+        statusControl.setValidators([Validators.required]);
+      } else {
+        statusControl.clearValidators();
+      }
+      statusControl.updateValueAndValidity();
+    });
   }
 
   onSubmit() {
-    this.service.save(this.bug).subscribe(result => this.router.navigate(["bug-list"]));
-    console.log(this.bug);
-  }
-
-  get statusIsInvalid () {
-    return this.bug.reporter === "QA" && this.bug.status && this.bug.status.length === 0;
+    this.service.save(this.bugFormGroup.value)
+      .subscribe(result => this.router.navigate(["bug-list"]));
   }
 
 }
