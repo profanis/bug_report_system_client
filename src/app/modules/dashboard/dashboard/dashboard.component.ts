1005b715ccd69ac791e206bc293bd624daaa812a
Fanis Prodromou <fanis@projectbeagle.com>
2018-05-17 13:45:41 +0300

    environment properties usage

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { environment } from "../../../../environments/environment";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
+  envName: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.envName = environment.nameOfTheEnvironment;
   }
 
 }
